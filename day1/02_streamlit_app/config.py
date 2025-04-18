b1c5d193f3134411a04f305ca6850fb1e0153247
Yuto KIZAWA <86343708+YutoKz@users.noreply.github.com>
2025-04-18 21:46:53 +0900

    Update config.py

--- day1/02_streamlit_app/config.py
+++ day1/02_streamlit_app/config.py
@@ -1,3 +1,4 @@
 # config.py
 DB_FILE = "chat_feedback.db"
-MODEL_NAME = "google/gemma-2-2b-jpn-it"
+#MODEL_NAME = "google/gemma-2-2b-jpn-it"
+MODEL_NAME = "cyberagent/DeepSeek-R1-Distill-Qwen-32B-Japanese"
